046ac126c13beaebb47e869b021ba210ede99f17
andrewkim1234 <145868382+andrewkim1234@users.noreply.github.com>
2024-01-20 13:53:26 -0800

    Splicing changes

--- server/server.js
+++ server/server.js
@@ -31,29 +31,45 @@ app.post('/process-video', upload.single('video'), async (req, res) => {
   }
 });
 
-function processVideo(inputBuffer, additionalVideoPath) {
-  return new Promise((resolve, reject) => {
-    fluentFFmpeg()
-      .input(inputBuffer)
-      .inputFormat('mp4')
-      .input(additionalVideoPath)
-      .inputFormat('mp4')
-      .complexFilter('[0:v][1:v]vstack=inputs=2[v]')
-      .on('end', () => resolve())
-      .on('error', (err) => reject(err))
-      .toFormat('mp4')
-      .outputOptions('-c:v libx264')  // Optional: Specify video codec
-      .on('data', (chunk) => {
-        // Save the output buffer chunks
-        resolve(chunk);
-      })
-      .on('end', () => {
-        // Signal the end of the buffer
-        resolve(null);
-      })
-      .run();
-  });
-}
+fluentFFmpeg({ source: 'NBA.mp4' })
+  .setStartTime(10)
+  .duration(10)
+  .on('start', function(commandLine) {
+    console.log("Processing Begun");
+  })
+  .on('error', function(err) {
+    console.error("Error occurred:", err);
+  })  
+  .on('end', function() {
+    console.log("Processing Completed");
+  })
+  .saveToFile("newVid.mp4");
+
+
+  function processVideo(inputBuffer, additionalVideoPath) {
+    return new Promise((resolve, reject) => {
+      fluentFFmpeg()
+        .input(inputBuffer)
+        .inputFormat('mp4')
+        .input(additionalVideoPath)
+        .inputFormat('mp4')
+        .complexFilter('[0:v][1:v]vstack=inputs=2[v]')
+        .on('end', () => resolve())
+        .on('error', (err) => reject(err))
+        .toFormat('mp4')
+        .outputOptions('-c:v libx264')  // Optional: Specify video codec
+        .on('data', (chunk) => {
+          // Save the output buffer chunks
+          resolve(chunk);
+        })
+        .on('end', () => {
+          // Signal the end of the buffer
+          resolve(null);
+        })
+        .run();  
+    });
+  }
+  
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
